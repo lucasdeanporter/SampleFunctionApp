820954936eea94fb0d98c1fd2f46c59e98e10c78
lucasdeanporter <120062051+lucasdeanporter@users.noreply.github.com>
2023-03-13 20:43:11 -0400

    Added a few key details
    Blob, File Share, and Azure SQL capabilities to start with, probably to break this app out of.

--- Fundamentals/Interfaces/ITime/ITimeRecord.cs
+++ Fundamentals/Interfaces/ITime/ITimeRecord.cs
@@ -1,5 +1,4 @@
-﻿using SampleFunctionApp.Fundamentals.Classes.IGeneral;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +9,8 @@ namespace SampleFunctionApp.Fundamentals.Interfaces.ITime
 {
     internal interface ITimeRecord : ITimeArchive
     {
-        public static DateTime EndTime { get => DateTime.UtcNow; }
-        public static DateTime StartTime { get => ITimeArchive.start; set => ITimeArchive.start = value; }
-        public static DateTime GetTimeNow => AppStart.;
-        static void OnStart(); // set time
-        public void OnEnd(); // set end
+        public static DateTime EndTime;
+        public static DateTime StartTime;
+        public static DateTime GetTimeNow => DateTime.UtcNow;
     }
 }
