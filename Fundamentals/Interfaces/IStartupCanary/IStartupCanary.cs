e2102bf998a04cc9de364cb37d9f21f876483dd4
lucasdeanporter <120062051+lucasdeanporter@users.noreply.github.com>
2023-03-13 23:45:59 -0400

    Create IStartupCanary.cs

---
+++ Fundamentals/Interfaces/IStartupCanary/IStartupCanary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFunctionApp.Fundamentals.Interfaces.IStartupCanary
+{
+    internal interface IStartupCanary
+    {
+    } 
+}
