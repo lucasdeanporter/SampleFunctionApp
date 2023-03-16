2d0bc96e202dfa61369fa10de41975b7dcb939ed
lucasdeanporter <120062051+lucasdeanporter@users.noreply.github.com>
2023-03-15 20:11:33 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace SampleFunctionApp
 {
+    // TODO: Async challenges, not a challenge: https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-scenarios#wait-for-multiple-tasks-to-complete
+    // https://en.wikipedia.org/wiki/Referential_transparency
+    // https://devblogs.microsoft.com/dotnet/understanding-the-whys-whats-and-whens-of-valuetask/
     internal class Startup
     {
     }
