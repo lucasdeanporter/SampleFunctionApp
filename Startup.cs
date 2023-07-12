3361a3becf7d9bce6d83bd8f6ba74d78856c37d4
lucasdeanporter <120062051+lucasdeanporter@users.noreply.github.com>
2023-07-12 09:00:20 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SampleFunctionApp.Fundamentals.Interfaces.Concepts;
 
-namespace SampleFunctionApp
+// TODO: Async challenges, not a challenge: https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-scenarios#wait-for-multiple-tasks-to-complete
+// https://en.wikipedia.org/wiki/Referential_transparency
+// https://devblogs.microsoft.com/dotnet/understanding-the-whys-whats-and-whens-of-valuetask/
+
+// https://learn.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
+[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+
+namespace MyNamespace
 {
-    // TODO: Async challenges, not a challenge: https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-scenarios#wait-for-multiple-tasks-to-complete
-    // https://en.wikipedia.org/wiki/Referential_transparency
-    // https://devblogs.microsoft.com/dotnet/understanding-the-whys-whats-and-whens-of-valuetask/
-    internal class Startup
+    public class Startup : FunctionsStartup
     {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        // https://learn.microsoft.com/en-us/dotnet/core/extensions/httpclient-factory#basic-usage
+            builder.Services.AddHttpClient();
+            /*
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddSingleton<IMyService>((s) => {
+                return new MyService();
+            });
+
+            builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+            */
+        }
     }
 }
